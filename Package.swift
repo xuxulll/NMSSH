eecae42b185180c065442defadb71c0a43c56dca
Ray <4958511+xuxulll@users.noreply.github.com>
2023-09-14 21:57:35 +0800

    update mac os version

--- Package.swift
+++ Package.swift
@@ -14,12 +14,13 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
+        .package(name: "CSSH", url: "https://github.com/DimaRU/Libssh2Prebuild.git", from: "1.11.0")
     ],
     targets: [
         .binaryTarget(
             name: "NMSSH",
             url: "https://github.com/xuxulll/NMSSH/releases/download/2.3.1-p6/NMSSH.xcframework.zip",
             checksum: "0ee3baba320106f0f4b576929ba04a5539782515c7db84365a49d4e8608a9240"
-        ),
+        )
     ]
 )
